59615f96e9f63159e9d7c2fa65af1947a7643d33
qurek9038 <71150179+qurek9038@users.noreply.github.com>
2020-10-09 16:08:16 -0400

    Update Main.java
    Question A3Q2 Completed

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *This program will correctly name the animal based on the choices giving the user yes or no answers
+ * @author Khazina
  */
 public class Main {
 
@@ -9,7 +10,27 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //Create the scanner for user input
+    Scanner input = new Scanner (System.in);
+
+    //Ask the user if the animal has feathers
+    System.out.println("Does the animal have feathers?");
+    String feathers = input.nextLine();
+    if (feathers.equals("yes")){
+      System.out.println("Does the animal swim?");
+      String feathersSwim = input.nextLine();
     
-  }
-}
+      if (feathersSwim.equals("yes")){
+        System.out.println("you have a Duck");
+      }else if (feathersSwim.equals("no")){
+        System.out.println("you have a Hen");
+
+      }
+    }else if (feathers.equals("no")){
+      System.out.println("Does the animal have legs?");
+      String feathersLegs = input.nextLine();
+
+      if (feathersLegs.equals("yes")){
+        System.out.println("you have a Lizard");
+      }else if (feathersLegs.equals("no")){
+        System.out.println("you have a Snake");
